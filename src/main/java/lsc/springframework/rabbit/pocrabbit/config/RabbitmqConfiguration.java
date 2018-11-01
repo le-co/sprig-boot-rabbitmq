890d963fa8a7d2adf3db4ea3b6f609ba7de300fd
leco <lcosta.sp.br@gmail.com>
2018-11-01 20:02:49 -0300

    without RabbitmqListener

--- src/main/java/lsc/springframework/rabbit/pocrabbit/config/RabbitmqConfiguration.java
+++ src/main/java/lsc/springframework/rabbit/pocrabbit/config/RabbitmqConfiguration.java
@@ -1,10 +1,11 @@
 package lsc.springframework.rabbit.pocrabbit.config;
 
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import lsc.springframework.rabbit.pocrabbit.services.amq.Receiver;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,5 +35,19 @@ public class RabbitmqConfiguration {
     @Bean
     Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(this.route);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(this.queue);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receive");
     }
 }
--- src/main/java/lsc/springframework/rabbit/pocrabbit/services/amq/Receiver.java
+++ src/main/java/lsc/springframework/rabbit/pocrabbit/services/amq/Receiver.java
@@ -1,14 +1,11 @@
 package lsc.springframework.rabbit.pocrabbit.services.amq;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
 
-    @RabbitListener(queues = "${leco.mq.queue}")
-    public void receive(@Payload String fileBody) throws InterruptedException {
+    public void receive(String fileBody) throws InterruptedException {
         Thread.sleep(2000);
         System.out.println("Received < " + fileBody + ">");
     }
